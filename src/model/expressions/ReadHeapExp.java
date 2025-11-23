4cfc14bb5de7b364b4d7691c14e1658100b7fa5c
mihai <cosmagiumihai1@outlook.com>
2025-11-23 16:54:46 +0200

    implementation of readheap expression + writeheap statement

---
+++ src/model/expressions/ReadHeapExp.java
@@ -0,0 +1,43 @@
+package model.expressions;
+
+import exception.MyException;
+import model.adt.MyIDictionary;
+import model.adt.MyIHeap;
+import model.values.IValue;
+import model.values.RefValue;
+
+public class ReadHeapExp implements IExp{
+    private IExp exp;
+
+    public ReadHeapExp(IExp exp) {
+        this.exp = exp;
+    }
+
+    @Override
+    public IValue eval(MyIDictionary<String, IValue> table, MyIHeap heap) throws MyException {
+        IValue val = exp.eval(table, heap);
+
+        if(!(val instanceof RefValue)) {
+            throw new MyException("Expression is not a reference!");
+        }
+
+        RefValue refVal = (RefValue) val;
+        int address = refVal.getAddr();
+
+        if(!heap.isDefined(address)) {
+            throw new MyException("Address " + address + " is not defined!");
+        }
+
+        return heap.getVal(address);
+    }
+
+    @Override
+    public IExp deepCopy() {
+        return new ReadHeapExp(exp.deepCopy());
+    }
+
+    @Override
+    public String toString() {
+        return "rH(" + exp.toString() + ")";
+    }
+}
---
+++ src/model/statements/WriteHeapStmt.java
@@ -0,0 +1,62 @@
+package model.statements;
+
+import exception.MyException;
+import model.PrgState;
+import model.adt.MyIDictionary;
+import model.adt.MyIHeap;
+import model.expressions.IExp;
+import model.types.RefType;
+import model.values.IValue;
+import model.values.RefValue;
+
+public class WriteHeapStmt implements IStmt{
+    private String varName;
+    private IExp exp;
+
+    public WriteHeapStmt(String varName, IExp exp) {
+        this.varName = varName;
+        this.exp = exp;
+    }
+
+    @Override
+    public PrgState execute(PrgState state) throws MyException {
+        MyIDictionary<String, IValue> symTable = state.getSymTable();
+        MyIHeap heap = state.getHeap();
+
+        if(!symTable.isDefined(varName)) {
+            throw new MyException("Variable " + varName + " is not defined");
+        }
+
+        IValue value = symTable.get(varName);
+        if(!(value.getType() instanceof RefType)) {
+            throw new MyException("Variable " + varName + " is not of type RefType");
+        }
+
+        RefValue refValue = (RefValue) value;
+        int address = refValue.getAddr();
+        if(!heap.isDefined(address)) {
+            throw new MyException("Address " + address + " is not defined");
+        }
+
+        IValue evaluated = exp.eval(symTable, heap);
+
+        RefType refType = (RefType) evaluated.getType();
+        if(!evaluated.getType().equals(refType.getInner())) {
+            throw new MyException("Expression type does not match variable reference type.");
+        }
+
+        heap.update(address, evaluated);
+
+        return null;
+    }
+
+    @Override
+    public IStmt deepCopy() {
+        return new WriteHeapStmt(varName, exp.deepCopy());
+    }
+
+    @Override
+    public String toString() {
+        return "wH(" + varName + ", " + exp.toString() + ")";
+    }
+}
